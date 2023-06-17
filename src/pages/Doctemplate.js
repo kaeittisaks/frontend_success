f7d198f46ef161f1ff96494198ca31789818a114
kaeittisak <129357373+kaeittisaks@users.noreply.github.com>
2023-06-17 18:35:20 +0700

    Update Doctemplate.js

--- src/pages/Doctemplate.js
+++ src/pages/Doctemplate.js
@@ -5,66 +5,11 @@ import axios from 'axios';
 
 
 const GenerateContract = () => {
-  const [employeeList, setEmployeeList] = useState([]);
-  const [selectedEmployee, setSelectedEmployee] = useState('');
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetchEmployeeList();
-  }, []);
-
-  const fetchEmployeeList = async () => {
-    try {
-      const response = await axios.get('https://jingjo-backend-customer.vercel.app/employee_list');
-      setEmployeeList(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleEmployeeChange = (e) => {
-    setSelectedEmployee(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post('https://jingjo-backend-customer.vercel.app/generate_contract', {
-        selected_employee: selectedEmployee,
-      });
-      setMessage(response.data);
-      const downloadLink = response.data;
-      window.location.href = downloadLink;
-    } catch (error) {
-      setMessage('เกิดข้อผิดพลาดในการสร้างสัญญา');
-    }
-  };
-
-
  
 
   return (
     <div>
-      <h1>Generate Contract</h1>
-      <form onSubmit={handleSubmit}>
-        <p htmlFor="selected_employee">เลือกรายชื่อที่ต้องการแสดงข้อมูล:</p>
-        <select
-          id="selected_employee"
-          value={selectedEmployee}
-          onChange={handleEmployeeChange}
-          required
-        >
-          <option value="">เลือก</option>
-          {employeeList.map((employee, index) => (
-            <option key={index} value={employee.employee_ID}>
-              {employee.fullNameEng}
-            </option>
-          ))}
-        </select>
-        <button type="submit">สร้างสัญญา</button>
-      </form>
-      <p>{message}</p>
+     .
     </div>
   );
 };
